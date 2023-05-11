a981f80da977f091f654cc44f28442c84f8aae81
diaabadr <diaabadr82@gmail.com>
2023-05-12 00:28:47 +0300

    ROC curve

--- face_recognition.py
+++ face_recognition.py
@@ -1,4 +1,9 @@
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 import numpy as np
+
 
 """
 description: Creating the PCA model
@@ -55,3 +60,20 @@ def test(test_img, weights, avg_face_vector, eigen_faces):
     index =  np.argmin(np.linalg.norm(test_weight - weights, axis=1))
 
     return index
+
+
+
+def plot_roc_curve(y_true, y_score):
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.show()
