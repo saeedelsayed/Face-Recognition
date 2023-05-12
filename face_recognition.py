9253e2fde5389bf08368453c991d054c1950c435
sherif <sherifahmedelsayed2@gmail.com>
2023-05-13 00:39:26 +0300

    add the code to the webcam
    detect faces from the webcam and recognize this face from the reference dataset

--- face_recognition.py
+++ face_recognition.py
@@ -8,17 +8,21 @@ import matplotlib.pyplot as plt
 from itertools import cycle
 
 
-"""
-description: Creating the PCA model
-parameters : face_vector ---> contain the training images where each column contain one image
-             of size (N^2 * 1)
-return value : weights ----> vector contain the similarity between each image and eigen vectors
-               avg_face_vector ----> the mean image ,used in testing
-               eigen_faces ----> contain eigen vectors ,used in testing
-
-"""
 
 def PCA(face_vector):
+    """
+    Creating the PCA model
+    
+    ## Parameters 
+    face_vector : contain the training images where each column contain one image of size (N^2 * 1)
+    
+    ## Returns
+    weights : vector contain the similarity between each image and eigen vectors
+
+    avg_face_vector : the mean image, used in testing
+
+    eigen_faces : contain eigen vectors, used in testing
+    """
     #get the mean image
     avg_face_vector = face_vector.mean(axis=1)
     avg_face_vector = avg_face_vector.reshape(face_vector.shape[0], 1)
@@ -46,24 +50,29 @@ def PCA(face_vector):
 
     return weights, avg_face_vector, eigen_faces
 
-"""
-description : recognize faces in test image
-parameters  : test_img ---> image to recognize faces in , it is in gray scale
-              weights ---> the weights resulting from PCA model
-              ave_face_vector ---> average face from the training images
-              eigen_faces ---> eigen vector resulting from PCA model
-return value : index ---> index of the matched image
-
-"""
-
 def test(test_img, weights, avg_face_vector, eigen_faces):
+    """
+    Recognize faces in test image
+
+    ## Parameters
+    test_img : image to recognize faces in , it is in gray scale
+
+    weights : the weights resulting from PCA model
+
+    ave_face_vector : average face from the training images
+
+    eigen_faces : eigen vector resulting from PCA model
+    
+    ## Returns
+    index : index of the matched image
+
+    """
     test_img = test_img.reshape(test_img.shape[0] * test_img.shape[1], 1)
     test_normalized_face_vector = test_img - avg_face_vector
     test_weight = (test_normalized_face_vector.T).dot(eigen_faces.T)
     index =  np.argmin(np.linalg.norm(test_weight - weights, axis=1))
 
     return index
-
 
 
 def plot_multiclass_roc(y_true, y_score, n_classes):
